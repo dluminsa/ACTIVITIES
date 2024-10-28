afec3243900345b85546914632dc65ba8103fafb
dluminsa <139705111+dluminsa@users.noreply.github.com>
2024-10-28 20:06:53 +0300

    Update ACTIVITIES.py

--- ACTIVITIES.py
+++ ACTIVITIES.py
@@ -4,6 +4,7 @@ from streamlit_gsheets import GSheetsConnection
 import traceback
 import streamlit as st
 import time
+import gspread
 import datetime as dt
 from datetime import datetime, date
 from google.oauth2.service_account import Credentials
