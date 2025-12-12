d299014c02a2adfa60271c52cc6dfb77920176e7
dluminsa <139705111+dluminsa@users.noreply.github.com>
2025-12-12 14:53:37 +0300

    Update 3_DASHBOARD.py

--- pages/3_DASHBOARD.py
+++ pages/3_DASHBOARD.py
@@ -45,6 +45,7 @@ dfb = dfb.drop_duplicates(subset = ['ID','DONE','DISTRICT', 'FACILITY', 'ACTIVIT
 
 file = r'PLANNED.csv'
 dfa = pd.read_csv(file)
+st.write(dfa)
 
 dfa= dfa[['DISTRICT', 'AREA','ACTIVITY', 'PLANNED', 'AMOUNT']]
 
